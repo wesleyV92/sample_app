42a4596d0114c0a1966af4e74e41afe6b0018f9b
Wesley <wesley.vandervijgh@student.uva.nl>
2014-01-14 13:05:48 -0500

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #get test of home to run properly
+  config.include Capybara::DSL
 end
